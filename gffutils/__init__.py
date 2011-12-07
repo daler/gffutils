815c2b8a804f82e216c33e4d946f9fe7aafd378b
daler <dalerr@niddk.nih.gov>
2011-12-07 15:26:25 -0500

    support for pickling Feature objects (needed for multiprocessing)

--- gffutils/__init__.py
+++ gffutils/__init__.py
@@ -1,3 +1,19 @@
 from gfffeature import Feature, GFFFile
 from db import GFFDBCreator, GTFDBCreator, FeatureDB
 from helpers import FeatureNotFoundError, clean_gff, inspect_featuretypes
+import copy_reg
+
+
+def feature_constructor(fields):
+    return Feature(*fields)
+
+
+def feature_reducer(feature):
+    return feature_constructor, (
+            (
+                feature.chrom, feature.source, feature.featuretype,
+                feature.start, feature.stop, feature.score, feature.strand,
+                feature.frame, feature._str_attributes, feature.filetype
+            ), )
+
+copy_reg.pickle(Feature, feature_reducer, feature_constructor)
