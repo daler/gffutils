cc65d452ac88d1626428f6c9b0a16fd7013366cf
daler <dalerr@niddk.nih.gov>
2011-12-21 17:31:32 -0500

    additions to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    print "-------------------------------------------------"
+    print "Please install Cython: (try 'pip install cython')"
+    print "-------------------------------------------------"
 
 setup(
         name='gffutils',
         cmdclass={'build_ext': build_ext},
-        ext_modules=[Extension('gfffeature', sources=['gffutils/gfffeature.pyx'])]
+        install_requires=['cython'],
+        ext_modules=[Extension('gfffeature', sources=['gffutils/gfffeature.pyx'])],
+        packages=['gffutils'],
+        author='Ryan Dale',
+        package_dir={'gffutils': 'gffutils'},
+        description="Work with GFF and GTF files in a flexible database framework",
+        author_email='dalerr@niddk.nih.gov',
+        url='none',
     )
