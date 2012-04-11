ab62cc6c92c2949259c71dbef8c2d61694a262d9
daler <dalerr@niddk.nih.gov>
2012-04-11 17:17:43 -0400

    autodetecting factory function for GTF/GFF databases

--- gffutils/__init__.py
+++ gffutils/__init__.py
@@ -1,3 +1,4 @@
+import os
 from gfffeature import Feature, GFFFile
 from db import GFFDBCreator, GTFDBCreator, FeatureDB
 from helpers import FeatureNotFoundError, clean_gff, inspect_featuretypes
@@ -17,3 +18,25 @@ def feature_reducer(feature):
             ), )
 
 copy_reg.pickle(Feature, feature_reducer, feature_constructor)
+
+
+def create_db(fn, dbfn, verbose=True, force=False):
+    """
+    Detects input format (GFF or GTF and calls the appropriate creation code,
+    resulting in a new database ready for use with FeatureDB.
+
+    `fn` is the filename of a GFF or GTF file
+
+    `dbfn` is the filename of the database to create.
+
+    Use `verbose=False` to disable progress info.
+
+    Use `force=True` to force overwrite of `dbfn` if it already exists.
+    """
+    first_feature = GFFFile(fn).next()
+    fmt = first_feature.filetype
+    if fmt == 'gff':
+        creator = GFFDBCreator(fn, dbfn, verbose=verbose)
+    else:
+        creator = GTFDBCreator(fn, dbfn, verbose=verbose)
+    creator.create()
