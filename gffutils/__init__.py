4231e11240b1360ae70a8d0dfcd4b7e76a419df1
daler <dalerr@niddk.nih.gov>
2013-02-25 17:57:25 -0500

    import helpers into __init__

--- gffutils/__init__.py
+++ gffutils/__init__.py
@@ -4,6 +4,7 @@ from gfffeature import Feature, GFFFile
 from db import GFFDBCreator, GTFDBCreator, FeatureDB
 from helpers import FeatureNotFoundError, clean_gff,\
         inspect_featuretypes, example_filename
+import helpers
 import copy_reg
 
 
