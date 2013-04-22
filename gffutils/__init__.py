49ed174b4524d845eb8663258b31114da9018d1f
daler <dalerr@niddk.nih.gov>
2013-04-22 18:56:46 -0400

    now use iterators.py instead of parser.py for iterating through data

--- gffutils/__init__.py
+++ gffutils/__init__.py
@@ -1,6 +1,6 @@
 from create import create_db
 from interface import FeatureDB
 from feature import Feature
+from iterators import FileIterator, StringIterator, FeatureIterator
 from helpers import example_filename, FeatureNotFoundError, DuplicateIDError
-from parser import Parser
 from version import version as __version__
