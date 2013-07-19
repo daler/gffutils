3b68e2a02c13ee2775e1b743634a1aabe3a4e736
daler <dalerr@niddk.nih.gov>
2013-07-19 17:53:49 -0400

    test for region()

--- gffutils/test/test.py
+++ gffutils/test/test.py
@@ -373,7 +373,22 @@ def test_sanitize_gff():
     print "Sanitized GFF successfully."
 
 
-    
+def test_region():
+    db_fname = gffutils.example_filename("gff_example1.gff3")
+    db = gffutils.create_db(db_fname, ":memory:")
+    all_in_region = list(db.region("chr1:4000000-5000000"))
+    all_minus = list(db.region("chr1:4000000-5000000:-"))
+    all_plus = list(db.region("chr1:4000000-5000000:+"))
+    all_unstranded = list(db.region("chr1:4000000-5000000:."))
+
+    out_of_range = list(db.region("nowhere:1-100"))
+
+    assert len(all_in_region) == 12
+    assert len(all_minus) == 12
+    assert len(all_plus) == 0
+    assert len(all_unstranded) == 0
+    assert len(out_of_range) == 0
+
 
 
 if __name__ == "__main__":
