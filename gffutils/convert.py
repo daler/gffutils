c745539711b0ae005fc169c1f7e7befa03bccca0
daler <dalerr@niddk.nih.gov>
2013-08-19 19:57:36 -0400

    docs for to_bed12

--- gffutils/convert.py
+++ gffutils/convert.py
@@ -5,6 +5,22 @@ Conversion functions that operate on :class:`FeatureDB` classes.
 def to_bed12(f, db, child_type='exon', name_field='ID'):
     """
     Given a top-level feature (e.g., transcript), construct a BED12 entry
+
+    Parameters
+    ----------
+    f : Feature object or string
+        This is the top-level feature represented by one BED12 line.  For
+        a canonical GFF or GTF, this will generally be a transcript.
+
+    db : a FeatureDB object
+        This is need to get the children for the feature
+
+    child_type : str
+        Featuretypes that will be represented by the BED12 "blocks".  Typically "exon".
+
+    name_field : str
+        Attribute to be used in the "name" field of the BED12 entry.  Usually
+        "ID" for GFF; "transcript_id" for GTF.
     """
     if isinstance(f, basestring):
         f = db[f]
