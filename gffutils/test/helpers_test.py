3128e1333d88cf3534e103a1a446d095c9310d26
Gabriel Pratt <gpratt@ucsd.edu>
2014-02-08 13:35:13 -0800

    fixed bug in merge_attributed, added tests to cover regression in the future

---
+++ gffutils/test/helpers_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from gffutils import helpers
+class Test(unittest.TestCase):
+    
+    def test_merge_attributes(self):
+        print "bar"
+        x = {'foo' : [1], "baz" : 1, "buz" : [1], "biz" : 1, "boo" : [1]}
+        y = {'bar' : [2], "baz" : 2, "buz" : [2], "biz" : 1, "boo" : [1]}
+        test = helpers.merge_attributes(x,y)
+        true = {'foo' : [1],
+                'bar' : [2],
+                "baz" : [1,2],
+                "boo" : [1],
+                "buz" : [1,2],
+                "biz" : [1]}
+        self.assertDictEqual(test, true) 
