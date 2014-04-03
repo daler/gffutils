227d87ddef7d18e3adde2a2cfc701687bac1b65b
daler <dalerr@niddk.nih.gov>
2014-04-03 13:52:11 -0400

    module-level import feature_from_line

--- gffutils/__init__.py
+++ gffutils/__init__.py
@@ -1,6 +1,6 @@
 from create import create_db
 from interface import FeatureDB
-from feature import Feature
+from feature import Feature, feature_from_line
 from iterators import FileIterator, UrlIterator, StringIterator, FeatureIterator
 from helpers import example_filename, FeatureNotFoundError, DuplicateIDError
 from version import version as __version__
