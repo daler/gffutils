0936b8a465c42e6da75a17d2a72de3ad8ee7dd57
daler <dalerr@niddk.nih.gov>
2014-08-08 14:38:59 -0400

    rm py2 print syntax from test

--- gffutils/test/helpers_test.py
+++ gffutils/test/helpers_test.py
@@ -23,10 +23,6 @@ class Test(unittest.TestCase):
     def test_merge_Attributes(self):
         f1 = feature.feature_from_line('chr2L . testing 1 10 . + . foo=1; baz=1; buz=1; biz=1; boo=1;', strict=False)
         f2 = feature.feature_from_line('chr2L . testing 1 10 . + . bar=2; baz=2; buz=2; biz=1; boo=1;', strict=False)
-
-        print f1,
-        print f1.attributes
-
         test = helpers.merge_attributes(f1.attributes, f2.attributes)
         true = {'foo': ['1'],
                 'bar': ['2'],
