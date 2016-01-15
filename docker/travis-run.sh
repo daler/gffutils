91c8493f557289754368a67225b0ca99d349d371
daler <dalerr@niddk.nih.gov>
2016-01-14 21:25:35 -0500

    fix filename typo

--- docker/travis-run.sh
+++ docker/travis-run.sh
@@ -51,7 +51,7 @@ for PYTHON in 2 3; do
 
     # Prepare for testing by installing nose for main tests, and biopython for
     # the biopython integration tests.  Then run 'em.
-    conda install -y nose --file optional-requirements
+    conda install -y nose --file optional-requirements.txt
     nosetests -x --with-doctest
 
     # Install tools and run doctests
