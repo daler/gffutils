c0f6f5be33f2518117ffd886a5aeef3b94788cf9
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-05 16:30:03 -0400

    install reqs via conda

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
-ls ~
+HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
 conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
+conda install --file $HERE/../requirements.txt
+conda install --file $HERE/../optional-requirements.txt
 ~/anaconda/bin/python setup.py install
