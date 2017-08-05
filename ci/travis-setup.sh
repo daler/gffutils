46b5228367a23bd8e8503e76ca9be7c7aa001a72
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-05 16:24:07 -0400

    switch channel order

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euo pipefail
+ls ~
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
-conda config --add channels conda-forge
 conda config --add channels defaults
+conda config --add channels conda-forge
 conda config --add channels bioconda
 ~/anaconda/bin/python setup.py install
