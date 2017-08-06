1d11ff6855f2df22b4918488491b5a24166995a3
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-06 19:50:55 -0400

    run a separate pip-install test

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -7,3 +7,11 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nosetests -x --with-doctest -a '!slow'
 conda install -y --file $HERE/../docs-requirements.txt
 (cd doc && make clean && make doctest)
+
+
+conda create -n new python=$TRAVIS_PYTHON_VERSION
+source activate new
+python setup.py sdist
+pip install dist/gffutils-*.tar.gz
+python -c 'import gffutils; print(gffutils.__version__)'
+source deactivate
