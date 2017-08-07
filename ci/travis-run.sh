3314fcb152223436ff95213b48121381c1539b54
Ryan Dale <dalerr@niddk.nih.gov>
2017-08-06 21:03:19 -0400

    clean up ci files

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -3,12 +3,13 @@
 set -eo pipefail
 set -x
 
+# Full test suite after conda-installing deps
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 nosetests -x --with-doctest -a '!slow'
 conda install -y --file $HERE/../docs-requirements.txt
 (cd doc && make clean && make doctest)
 
-
+# Fresh environment, pip-installed from just-created sdist tarball
 conda create -y -n new python=$TRAVIS_PYTHON_VERSION
 source activate new
 python setup.py sdist
--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -4,8 +4,15 @@ HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/anaconda
 export PATH=~/anaconda/bin:$PATH
+
 conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
-conda install -y --file $HERE/../requirements.txt --file $HERE/../optional-requirements.txt nose python=$TRAVIS_PYTHON_VERSION
+
+conda install -y \
+    --file $HERE/../requirements.txt \
+    --file $HERE/../optional-requirements.txt \
+    nose \
+    python=$TRAVIS_PYTHON_VERSION
+
 python setup.py install
