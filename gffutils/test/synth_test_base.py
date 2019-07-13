26a0dc2edee9a4c4a038fd4fce5a8d6c46e2447b
Nolan Woods <nolan_w@sfu.ca>
2019-07-12 18:25:22 -0700

    Add tests for merge and merge_all

---
+++ gffutils/test/synth_test_base.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+import gffutils
+
+synthetic_path = 'data/synthetic.gff3'
+num_synthetic_features = 18
+num_synthetic_overlap = 13
+
+
+class TestWithSynthDB(TestCase):
+    def setUp(self) -> None:
+        self.db = gffutils.create_db(synthetic_path, ":memory:", merge_strategy='create_unique')  # type: gffutils.FeatureDB
+        self.assertEqual(num_synthetic_features, self.db.count_features_of_type())
+
+    def _dump_db(self):
+        return '\n'.join(map(str, self.db.all_features()))
