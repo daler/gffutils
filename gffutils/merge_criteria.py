0ef975dbd5b83f29c7ef44e8a24d366fb5b0d7bf
Nolan Woods <nolan@i2labs.ca>
2019-12-29 16:21:10 -0800

    Revert annotations for py2 compat

--- gffutils/merge_criteria.py
+++ gffutils/merge_criteria.py
@@ -1,51 +1,51 @@
 """
 Merge criteria used by FeatureDB merge() and merge_all()
 """
-from gffutils.feature import Feature
+from gffutils import Feature
 
 
-def seqid(acc: Feature, cur: Feature, components: [Feature]):
+def seqid(acc, cur, components):
     return cur.seqid == acc.seqid
 
 
-def strand(acc: Feature, cur: Feature, components: [Feature]):
+def strand(acc, cur, components):
     return acc.strand == cur.strand
 
 
-def feature_type(acc: Feature, cur: Feature, components: [Feature]):
+def feature_type(acc, cur, components):
     return acc.featuretype == cur.featuretype
 
 
-def exact_coordinates_only(acc: Feature, cur: Feature, components: [Feature]):
+def exact_coordinates_only(acc, cur, components):
     return cur.start == acc.start and cur.stop == acc.end
 
 
-def overlap_end_inclusive(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_end_inclusive(acc, cur, components):
     return acc.start <= cur.start <= acc.end + 1
 
 
-def overlap_start_inclusive(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_start_inclusive(acc, cur, components):
     return acc.start <= cur.end + 1 <= acc.end + 1
 
 
-def overlap_any_inclusive(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_any_inclusive(acc, cur, components):
     return acc.start <= cur.start <= acc.end + 1 or acc.start <= cur.end + 1 <= acc.end + 1
 
 
-def overlap_end_threshold(threshold: int):
-    def partial(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_end_threshold(threshold):
+    def partial(acc, cur, components):
         return acc.start <= cur.start <= acc.end + threshold
     return partial
 
 
-def overlap_start_threshold(threshold: int):
-    def partial(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_start_threshold(threshold):
+    def partial(acc, cur, components):
         return acc.start - threshold <= cur.end + 1 <= acc.end + 1
 
     return partial
 
 
-def overlap_any_threshold(threshold: int):
-    def partial(acc: Feature, cur: Feature, components: [Feature]):
+def overlap_any_threshold(threshold):
+    def partial(acc, cur, components):
         return acc.start - threshold <= cur.end + 1 <= acc.end + 1 or acc.start <= cur.start <= acc.end + threshold
     return partial
