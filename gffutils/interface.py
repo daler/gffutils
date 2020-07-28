86a84177cd0a8714cdfd5af4110c9315fb45754f
Husen M. Umer <husensofteng@users.noreply.github.com>
2020-07-28 14:58:50 +0200

    fixed bug mentioned in issue #129
    report features that overlap a given region even partially or at boundaries.

--- gffutils/interface.py
+++ gffutils/interface.py
@@ -645,13 +645,23 @@ class FeatureDB(object):
             args.append(seqid)
         if start is not None:
             start = int(start)
-            position_clause.append('start %s ?' % start_op)
-            args.append(start)
         if end is not None:
             end = int(end)
-            position_clause.append('end %s ?' % end_op)
-            args.append(end)
-
+        
+        if start and end and not completely_within:
+            "get features overlapping the region partially or completely"
+            position_clause.append('''( ({region_start} <= start AND {region_end} >= start) OR 
+                                    ({region_start} >= start AND {region_end} <= end) OR 
+                                    ({region_start} <= end AND {region_end} >= end) )'''.format(
+                                        region_start=start, region_end=end))
+        else:
+            if start:
+                position_clause.append('start %s ?' % start_op)
+                args.append(start)
+            if end:
+                position_clause.append('end %s ?' % end_op)
+                args.append(end)
+        
         position_clause = ' AND '.join(position_clause)
 
         # Only use bins if we have defined boundaries and completely_within is
