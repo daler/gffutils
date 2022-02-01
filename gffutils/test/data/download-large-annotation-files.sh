fd7825d4ae27d0bd7ac4ec1561f3a3ec2b8e0f76
daler <dalerr@niddk.nih.gov>
2022-01-31 19:33:55 -0500

    update url for large file tests

--- gffutils/test/data/download-large-annotation-files.sh
+++ gffutils/test/data/download-large-annotation-files.sh
@@ -2,7 +2,6 @@
 # to gffutils/test/data/ directory.
 
 cd $(dirname $0)
-wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_mouse/release_M8/gencode.vM8.annotation.gff3.gz
-gzip -d gencode.vM8.annotation.gff3.gz
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M28/gencode.vM28.annotation.gtf.gz
 wget ftp://ftp.ensembl.org/pub/release-83/gff3/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.83.gff3.gz
 gzip -d Saccharomyces_cerevisiae.R64-1-1.83.gff3.gz
