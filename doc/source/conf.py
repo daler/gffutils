644d6c526b1e13482bf7eae8e308c04f19219d60
Ryan Dale <ryan.dale@nih.gov>
2022-06-04 17:29:33 -0400

    finish porting over previous conf.py items

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -43,26 +43,15 @@ extensions = [
 doctest_global_setup = """
 import gffutils
 """
-
+default_role = "literal"
+version = gffutils.__version__
+release = gffutils.__version__
 autoapi_dirs = ['../../gffutils']
 autoapi_generate_api_docs = False
-
-# Add any paths that contain templates here, relative to this directory.
+pygments_style = 'sphinx'
+issue_tracker_url = 'https://github.com/daler/gffutils/issues/{issue}'
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
